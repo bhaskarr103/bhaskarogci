806a854c04a62fc5cdcf157827baaec65c715d9f
psyche#br <123258478+bhaskarr103@users.noreply.github.com>
2024-03-29 12:47:58 +0530

    Add files via upload

---
+++ portfolionew.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Data Science Learner", "Data Analyst"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
